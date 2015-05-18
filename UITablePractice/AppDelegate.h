4fe7ad7b623afb4e8f923f1441b42aef217aa827
Sonny <sonny@Sonnys-Mac-Pro.local>
2015-05-18 09:28:22 -0700

    Initial Commit

---
+++ UITablePractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITablePractice
+//
+//  Created by Sonny on 5/18/15.
+//  Copyright (c) 2015 Sonny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
